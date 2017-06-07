4e80f7d1681469f9a84da77ca2881ea47f31e640
quasarua <Sergey.Romashchenko@gmail.com>
2017-06-07 17:43:08 +0300

    Update PathCollector.java

--- src/main/java/reflection/PathCollector.java
+++ src/main/java/reflection/PathCollector.java
@@ -1,53 +1,26 @@
 package reflection;
 
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
- * Created by Quasar on 3/22/2017.
+ * Created by Quasar on 5/29/2017.
  */
+public class PathCollect {
+    public static Set coll(String cpElements) throws IOException {
 
-        import java.io.File;
-        import java.io.IOException;
-        import java.nio.file.*;
-        import java.nio.file.attribute.BasicFileAttributes;
-        import java.util.*;
+        Set<String> paths = new HashSet<>();
+        String[] classPathElements = System
+                .getProperty("java.class.path")
+                .split(System.getProperty("path.separator"));
 
-
-public class PathCollector {
-
-    private static final PathMatcher CLASS_MATCHER = FileSystems.getDefault().getPathMatcher("glob:**.class");
-    private static final PathMatcher JAR_MATCHER = FileSystems.getDefault().getPathMatcher("glob:**.jar");
-
-    public static void collect(final Path parentPath, final Set<String> results) throws IOException {
-
-        Files.walkFileTree(parentPath, new OnErrorFileVisitor() {
-            @Override
-            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                if (CLASS_MATCHER.matches(file)) {
-                    Path rel = parentPath.relativize(file);
-                    String pkg = rel
-                            .toString()
-                            .replaceAll(".class$", "")
-                            .replaceAll("/", ".");
-                    results.add(pkg);
-                } else if (JAR_MATCHER.matches(file)) {
-                    try (FileSystem jar = FileSystems.newFileSystem(file, null)) {
-                        collect(jar.getPath("/"), results);
-                    }
-                }
-                return FileVisitResult.CONTINUE;
-            }
-        });
-    }
-
-    private static class OnErrorFileVisitor extends SimpleFileVisitor<Path> {
-        @Override
-        public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
-            if (exc != null) {
-                System.out.println("Exception on path: " + file);
-                System.out.println(exc.getMessage());
-                System.out.println(Arrays.toString(exc.getStackTrace()));
-            }
-
-            return FileVisitResult.CONTINUE;
+        for (String element : classPathElements) {
+            PathCollector.collect(Paths.get(element), paths);
         }
+
+        return paths;
+
     }
 }
